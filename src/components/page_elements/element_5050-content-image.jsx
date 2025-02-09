deec4fb79e8f58e1aae0d967f9d036cc49474745
Ally Garton <alexgarton@me.com>
2025-02-09 20:34:40 +0000

    started building 50/50 block

--- src/components/page_elements/element_5050-content-image.jsx
+++ src/components/page_elements/element_5050-content-image.jsx
@@ -1,1 +1,26 @@
-import '../../assets/styles/page_elements/element_5050-content-image.css';
+import PropTypes from "prop-types";
+import image from "../../assets/images/inb.jpg";
+
+const ContentImageSplit = ({ title, text, imageSrc, anchor, reversed }) => {
+    return (
+        <section className={`content-image-split reversed-${reversed}`} id={anchor}>
+            <div className="content-image-split__img-col">
+                <img className="content-image-split__img" src={image} alt=""/>
+            </div>
+            <div className="content-image-split__content-col">
+                <h2 className="content-image-split__title">{title}</h2>
+                <p className="content-image-split__text">{text} {imageSrc}</p>
+            </div>
+        </section>
+    );
+}
+
+ContentImageSplit.propTypes = {
+    title: PropTypes.string,
+    text: PropTypes.string,
+    imageSrc: PropTypes.string,
+    anchor: PropTypes.string,
+    reversed: PropTypes.bool
+};
+
+export default ContentImageSplit;
--- src/pages/education.jsx
+++ src/pages/education.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import PropTypes from "prop-types";
+import ContentImageSplit from "../components/page_elements/element_5050-content-image.jsx";
 
 export default function Education({ setTitle }) {
     useEffect(() => {
@@ -7,10 +8,39 @@ export default function Education({ setTitle }) {
         document.title = "Education - My Website";
     }, [setTitle]);
 
-    return (
-        <section>
+    const blocks = [
+        {
+            id: 1,
+            title: 'Lorem ipsum dolor',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies cursus risus sit amet cursus. Aliquam consequat sollicitudin auctor. Aliquam diam lorem, eleifend at blandit et, suscipit sit amet ante. Nullam a tincidunt augue, gravida dictum est. Vestibulum gravida quis tellus vitae posuere.',
+            image: '/',
+            anchor: 'gcses',
+            reversed: false
+        },
+        {
+            id: 2,
+            title: 'Lorem ipsum dolor',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies cursus risus sit amet cursus. Aliquam consequat sollicitudin auctor. Aliquam diam lorem, eleifend at blandit et, suscipit sit amet ante. Nullam a tincidunt augue, gravida dictum est. Vestibulum gravida quis tellus vitae posuere.',
+            image: '/',
+            anchor: 'alevels',
+            reversed: true
+        },
+        {
+            id: 3,
+            title: 'Lorem ipsum dolor',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies cursus risus sit amet cursus. Aliquam consequat sollicitudin auctor. Aliquam diam lorem, eleifend at blandit et, suscipit sit amet ante. Nullam a tincidunt augue, gravida dictum est. Vestibulum gravida quis tellus vitae posuere.',
+            image: '/',
+            anchor: 'bachelors',
+            reversed: false
+        },
+    ];
 
-        </section>
+    return (
+        <>
+            {blocks.map((item) => (
+                <ContentImageSplit key={item.id} title={item.title} text={item.text} anchor={item.anchor} reversed={item.reversed} />
+            ))}
+        </>
     )
 }
 
